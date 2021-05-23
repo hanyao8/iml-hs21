e27986395b3b35c94b1f6f226980727a82a439c4
hanyao8 <choong.hanyao@gmail.com>
2021-05-23 12:29:23 +0200

    siamese model 2

--- task4/task4_models.py
+++ task4/task4_models.py
@@ -42,5 +42,37 @@ def create_siamese():
 
     return (siamese_network)
 
+def create_siamese2():
+    feature_cnn = MobileNetV2(weights="imagenet",
+                              input_shape=(224,224,3),
+                              include_top=False)
+    
+    for layer in feature_cnn.layers:
+        layer.trainable = False
+
+    flatten = layers.Flatten()(feature_cnn.output)
+    dense1 = layers.Dense(512,activation="relu")(flatten)
+    dense1 = layers.BatchNormalization()(dense1)
+    dense2 = layers.Dense(256,activation="relu")(dense1)
+    dense2 = layers.BatchNormalization()(dense2)
+    output = layers.Dense(256)(dense2)
+    embedding = Model(feature_cnn.input,output,name="Embedding")
+
+    anchor_input = layers.Input(name="anchor", shape=(224,224,3))
+    positive_input = layers.Input(name="positive", shape=(224,224,3))
+    negative_input = layers.Input(name="negative", shape=(224,224,3))
+
+    distances = custom_objects.DistanceLayer()(
+        embedding(mobilenet.preprocess_input(anchor_input)),
+        embedding(mobilenet.preprocess_input(positive_input)),
+        embedding(mobilenet.preprocess_input(negative_input)),
+    )
+
+    siamese_network = Model(
+    inputs=[anchor_input, positive_input, negative_input], outputs=distances
+    )
+
+    return (siamese_network)
+
 
 
