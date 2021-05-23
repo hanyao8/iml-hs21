3991de9170dea728a2fe91ee985d2b6950e80dfc
hanyao8 <choong.hanyao@gmail.com>
2021-05-23 06:18:30 +0200

    models code reorganization

---
+++ task4/custom_objects.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+  
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import metrics
+from keras.models import Model
+
+###Siamese Network approach
+
+class DistanceLayer(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def call(self, anchor, positive, negative):
+        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+
+
+class SiameseModel(Model):
+    """
+    The triplet loss is defined as:
+       L(A, P, N) = max(‖f(A) - f(P)‖² - ‖f(A) - f(N)‖² + margin, 0)
+    """
+
+    def __init__(self, siamese_network, margin=0.5):
+        super(SiameseModel, self).__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.loss_tracker = metrics.Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            loss = self._compute_loss(data)
+
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        self.optimizer.apply_gradients(
+            zip(gradients, self.siamese_network.trainable_weights)
+        )
+
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, data):
+        loss = self._compute_loss(data)
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def _compute_loss(self, data):
+        ap_distance, an_distance = self.siamese_network(data)
+        loss = ap_distance - an_distance
+        loss = tf.maximum(loss + self.margin, 0.0)
+        return loss
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+
+
--- task4/task4_models.py
+++ task4/task4_models.py
@@ -8,17 +8,11 @@ from keras.models import Model
 from keras.applications import MobileNetV2
 from keras.applications import mobilenet
 
+import custom_objects
+
 ###Siamese Network approach
 
-class DistanceLayer(layers.Layer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    def call(self, anchor, positive, negative):
-        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
-        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
-        return (ap_distance, an_distance)
-
-def create_siamese1():
+def create_siamese():
     feature_cnn = MobileNetV2(weights="imagenet",
                               input_shape=(224,224,3),
                               include_top=False)
@@ -36,7 +30,7 @@ def create_siamese1():
     positive_input = layers.Input(name="positive", shape=(224,224,3))
     negative_input = layers.Input(name="negative", shape=(224,224,3))
 
-    distances = DistanceLayer()(
+    distances = custom_objects.DistanceLayer()(
         embedding(mobilenet.preprocess_input(anchor_input)),
         embedding(mobilenet.preprocess_input(positive_input)),
         embedding(mobilenet.preprocess_input(negative_input)),
@@ -49,46 +43,4 @@ def create_siamese1():
     return (siamese_network)
 
 
-class SiameseModel(Model):
-    """
-    The triplet loss is defined as:
-       L(A, P, N) = max(‖f(A) - f(P)‖² - ‖f(A) - f(N)‖² + margin, 0)
-    """
-
-    def __init__(self, siamese_network, margin=0.5):
-        super(SiameseModel, self).__init__()
-        self.siamese_network = siamese_network
-        self.margin = margin
-        self.loss_tracker = metrics.Mean(name="loss")
-
-    def call(self, inputs):
-        return self.siamese_network(inputs)
-
-    def train_step(self, data):
-        with tf.GradientTape() as tape:
-            loss = self._compute_loss(data)
-
-        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
-
-        self.optimizer.apply_gradients(
-            zip(gradients, self.siamese_network.trainable_weights)
-        )
-
-        self.loss_tracker.update_state(loss)
-        return {"loss": self.loss_tracker.result()}
-
-    def test_step(self, data):
-        loss = self._compute_loss(data)
-        self.loss_tracker.update_state(loss)
-        return {"loss": self.loss_tracker.result()}
-
-    def _compute_loss(self, data):
-        ap_distance, an_distance = self.siamese_network(data)
-        loss = ap_distance - an_distance
-        loss = tf.maximum(loss + self.margin, 0.0)
-        return loss
-
-    @property
-    def metrics(self):
-        return [self.loss_tracker]
 
