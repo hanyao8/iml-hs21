80a5a27387b532c37385b44e59c18e33bf24e49e
hanyao8 <choong.hanyao@gmail.com>
2021-05-27 20:07:02 +0200

    fix multitasking

--- task4/custom_objects_2.py
+++ task4/custom_objects_2.py
@@ -115,7 +115,6 @@ class SiameseModel5(Model):
 
 
     def _compute_triplet_loss(self,d_pred,y_true):
-        an_distance,ap_distance = d_pred
         triplet_loss = ap_distance - an_distance
 
         y_true = tf.cast(y_true,tf.float32)
@@ -124,7 +123,6 @@ class SiameseModel5(Model):
         return triplet_loss
 
     def _compute_triplet_loss_2(self,d_pred,y_true):
-        an_distance,ap_distance = d_pred
         triplet_loss = ap_distance - an_distance
         triplet_loss = tf.maximum(triplet_loss + self.margin, 0.0)
 
@@ -136,7 +134,6 @@ class SiameseModel5(Model):
         return triplet_loss
 
     def _compute_binary_loss(self,d_pred,y_true):
-        an_distance,ap_distance = d_pred
         Z = tf.math.exp(-1.0*an_distance)+tf.math.exp(-1.0*ap_distance)
         phat = tf.math.exp(-1.0*ap_distance)/Z
         bce = self.bce_op(y_true,phat)
@@ -147,7 +144,6 @@ class SiameseModel5(Model):
         return loss
 
     def _compute_acc(self, d_pred, y_true):
-        an_distance,ap_distance = d_pred
         y_pred = an_distance - ap_distance
         y_pred = tf.math.sign(y_pred)
         y_pred = tf.maximum(y_pred,0.0)
@@ -232,3 +228,128 @@ class SiameseModel6(Model):
     def metrics(self):
         return [self.loss_tracker,self.pos_frac_tracker]
 
+
+
+
+
+class SiameseModel7(Model):
+    def __init__(self, siamese_network, margin=0.5, mt_weights=[]):
+        super(SiameseModel7, self).__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.lambda_1 = mt_weights[0]
+        self.lambda_2 = mt_weights[1]
+
+        self.using_labels = True
+        self.bce_op = tf.keras.losses.BinaryCrossentropy()
+        self.acc_op = tf.keras.metrics.Accuracy()
+        self.loss_tracker = metrics.Mean(name="loss")
+        self.triplet_loss_tracker = metrics.Mean(name="triplet_loss")
+        self.binary_loss_tracker = metrics.Mean(name="binary_loss")
+        self.acc_tracker = metrics.Mean(name="acc")
+
+    def call(self, inputs):
+        if len(inputs) == 2:
+            return self.siamese_network(inputs[0])
+        elif len(inputs) == 3:
+            return self.siamese_network(inputs)
+        elif len(inputs) == 4:
+            return self.siamese_network(
+                    (inputs[0],inputs[1],inputs[2])
+                    )
+        else:
+            raise(Exception)
+
+    def train_step(self, data):
+        if self.using_labels:
+            x,y = data
+        else:
+            x = data
+
+        with tf.GradientTape() as tape:
+            ap_distance, an_distance = self.siamese_network(x)
+            d_pred = (ap_distance,an_distance)
+            triplet_loss = self._compute_triplet_loss(d_pred,y)
+            binary_loss = self._compute_binary_loss(d_pred,y)
+            loss = self._compute_loss(triplet_loss,binary_loss)
+
+        acc = self._compute_acc(d_pred,y)
+
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        self.optimizer.apply_gradients(
+            zip(gradients, self.siamese_network.trainable_weights)
+        )
+
+        self.loss_tracker.update_state(loss)
+        self.acc_tracker.update_state(acc)
+        self.triplet_loss_tracker.update_state(triplet_loss)
+        self.binary_loss_tracker.update_state(binary_loss)
+        return {"loss": self.loss_tracker.result(),
+                "acc": self.acc_tracker.result(),
+                "triplet_loss": self.triplet_loss_tracker.result(),
+                "binary_loss": self.binary_loss_tracker.result()
+                }
+
+    def test_step(self, data):
+        if self.using_labels:
+            x,y = data
+        else:
+            x = data
+
+        ap_distance, an_distance = self.siamese_network(x)
+        d_pred = (ap_distance,an_distance)
+
+        triplet_loss = self._compute_triplet_loss(d_pred,y)
+        binary_loss = self._compute_binary_loss(d_pred,y)
+        loss = self._compute_loss(triplet_loss,binary_loss)
+        acc = self._compute_acc(d_pred,y)
+
+        self.loss_tracker.update_state(loss)
+        self.acc_tracker.update_state(acc)
+        self.triplet_loss_tracker.update_state(triplet_loss)
+        self.binary_loss_tracker.update_state(binary_loss)
+        return {"loss": self.loss_tracker.result(),
+                "acc": self.acc_tracker.result(),
+                "triplet_loss": self.triplet_loss_tracker.result(),
+                "binary_loss": self.binary_loss_tracker.result()
+                }
+
+
+    def _compute_triplet_loss(self,d_pred,y_true):
+        ap_distance,an_distance = siamese_network(d_pred)
+        triplet_loss = ap_distance - an_distance
+        y_true = tf.reshape(y_true,shape=y_pred.shape)
+        y_true = tf.cast(y_true,tf.float32)
+        triplet_loss = tf.math.multiply(triplet_loss,(y_true*2.0)-1.0)
+        triplet_loss = tf.maximum(triplet_loss + 1.0, 0.0)
+        return triplet_loss
+
+    def _compute_binary_loss(self,d_pred,y_true):
+        ap_distance,an_distance = siamese_network(d_pred)
+        y_true = tf.reshape(y_true,shape=y_pred.shape)
+        y_true = tf.cast(y_true,tf.float32)
+        Z = tf.math.exp(-1.0*an_distance)+tf.math.exp(-1.0*ap_distance)
+        phat = tf.math.exp(-1.0*ap_distance)/Z
+        bce = -y_true*tf.math.log(phat)-(1-y_true)*tf.math.log(1-phat)
+        return bce
+
+    def _compute_loss(self, triplet_loss, binary_loss):
+        loss = self.lambda_1*triplet_loss + self.lambda_2*binary_loss
+        return loss
+
+    def _compute_acc(self, d_pred, y_true):
+        ap_distance,an_distance = d_pred
+        y_pred = an_distance - ap_distance
+        y_pred = tf.math.sign(y_pred)
+        y_true = tf.reshape(y_true,shape=y_pred.shape)
+        y_true = tf.cast(y_true,tf.float32)
+        y_true = y_true*2.0-1.0
+        acc = tf.multiply(y_pred,y_true)
+        acc = tf.maximum(acc,0.0)
+        return acc
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker,self.acc_tracker,
+                self.triplet_loss_tracker,self.binary_loss_tracker]
