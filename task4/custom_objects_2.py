07b9fc82aaefd6f41337f58e1bf0b6f9b700b3a6
hanyao8 <choong.hanyao@gmail.com>
2021-05-27 20:19:13 +0200

    fix bug

--- task4/custom_objects_2.py
+++ task4/custom_objects_2.py
@@ -317,7 +317,7 @@ class SiameseModel7(Model):
 
 
     def _compute_triplet_loss(self,d_pred,y_true):
-        ap_distance,an_distance = self.siamese_network(d_pred)
+        ap_distance,an_distance = d_pred
         triplet_loss = ap_distance - an_distance
         y_true = tf.reshape(y_true,shape=y_pred.shape)
         y_true = tf.cast(y_true,tf.float32)
@@ -326,7 +326,7 @@ class SiameseModel7(Model):
         return triplet_loss
 
     def _compute_binary_loss(self,d_pred,y_true):
-        ap_distance,an_distance = self.siamese_network(d_pred)
+        ap_distance,an_distance = d_pred
         y_true = tf.reshape(y_true,shape=y_pred.shape)
         y_true = tf.cast(y_true,tf.float32)
         Z = tf.math.exp(-1.0*an_distance)+tf.math.exp(-1.0*ap_distance)
